e9bfce6a7a53fe3754a074ceb5dd3d7b1fc86476
GAVANA-boxing <enkhmanlai.gg11@gmail.com>
2026-05-03 15:14:33 +0900

    Save localized reels route and lockfile

--- app/[locale]/reels/page.js
+++ app/[locale]/reels/page.js
@@ -1,33 +1,3 @@
-import { Suspense } from "react";
-import ReelsContent from "@/app/reels/ReelsContent";
+import ReelsPage from "../../reels/page";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-function LoadingFallback() {
-  return (
-    <div style={{
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100vw",
-      height: "100vh",
-      background: "#000",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      color: "#fff",
-      fontSize: 16,
-    }}>
-      Loading reels...
-    </div>
-  );
-}
-
-export default function LocalizedReelsPage() {
-  return (
-    <Suspense fallback={<LoadingFallback />}>
-      <ReelsContent />
-    </Suspense>
-  );
-}
+export default ReelsPage;
